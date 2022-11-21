57bbcc08ef7506a758fe7239e6cd183669016c18
OHeckIsThatSam <sam@scrooby.net>
2022-11-21 14:48:54 +0000

    Trigger Change and Logs Added

---
+++ AAS/DatabaseTriggers/SalaryUpdateTrigger.sql
@@ -0,0 +1,14 @@
+﻿CREATE TRIGGER [salary_trigger] ON [dbo].[BankAccount]
+	FOR UPDATE
+	AS IF (UPDATE(Salary))
+	BEGIN
+		SET NOCOUNT ON
+		DECLARE @AccountId int = (SELECT BankAccountID FROM deleted)
+		DECLARE @Salary float = (SELECT Salary FROM BankAccount WHERE BankAccountID = @AccountId)
+		IF (@Salary >= 30000)
+		BEGIN
+			UPDATE BankAccount
+			SET Overdraft = (@Salary * 0.1)
+			WHERE BankAccountID = @AccountId
+		END
+	END
---
+++ AAS/Pages/Logs/List.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AAS.Data;
+using AAS.Models;
+
+namespace AAS.Pages.Logs
+{
+    public class ListModel : PageModel
+    {
+        private readonly AAS.Data.Context _context;
+
+        public ListModel(AAS.Data.Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Log> Log { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (HttpContext.Session.GetString("employeeId") == null) Response.Redirect("../Login");
+
+            if (_context.Logs != null)
+            {
+                Log = await _context.Logs
+                .Include(l => l.Customer)
+                .Include(l => l.Employee).ToListAsync();
+            }
+        }
+    }
+}
